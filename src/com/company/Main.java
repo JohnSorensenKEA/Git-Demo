d53892e3f20756f8a7faf2ae2f18cfce95e4c7d7
John <john383p@stud.kea.dk>
2020-09-01 14:09:33 +0200

    Second commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,4 +5,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world");
     }
+
+    public static void second()
+    {
+        System.out.println("Hello my friend");
+    }
 }
